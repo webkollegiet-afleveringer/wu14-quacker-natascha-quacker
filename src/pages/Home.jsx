1cf12a01b60ef8ce76481b2cfb180827c3ed2f1d
NataschaBGB <nannabjerning@hotmail.com>
2026-04-21 09:54:29 +0200

    add: Pages to add to nav bar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import '../style/_base.sass';
+
+
+export default function Home() {
+  return (
+    <section className="home-page">
+      <h1>Home</h1>
+    </section>
+  )
+}
---
+++ src/pages/Notifications.jsx
@@ -0,0 +1,10 @@
+import '../style/_base.sass';
+
+
+export default function Notifications() {
+  return (
+    <section className="notifications-page">
+      <h1>Notifications</h1>
+    </section>
+  )
+}
